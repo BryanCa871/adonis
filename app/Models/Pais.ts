959a34f82444bc693ca4620321256eebc2f7d24b
BryanCa871 <bryansoe871@gmail.com>
2023-03-07 00:41:06 -0600

    a

--- app/Models/Pais.ts
+++ app/Models/Pais.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
-export default class Pai extends BaseModel {
+export default class Pais extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
