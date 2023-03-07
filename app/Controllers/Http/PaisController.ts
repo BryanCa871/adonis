91b5dc4fb854c59b528a50ed6156ca6154c67250
BryanCa871 <bryansoe871@gmail.com>
2023-03-07 01:17:22 -0600

    a

--- app/Controllers/Http/PaisController.ts
+++ app/Controllers/Http/PaisController.ts
@@ -1,13 +1,4 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 
 export default class PaisController {
-    public async SeleccionarPais(){
-        const Database = use('Database')
-
-        const results = await Database.table('users').select('*')
-
-        
-
-    }
 }
